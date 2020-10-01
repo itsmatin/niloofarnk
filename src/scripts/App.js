b013d9889e6746aefb9597cab3643478387db10a
Matin_ <matinnikookar@gmail.com>
2020-10-01 15:56:50 +0800

    initial commit

---
+++ src/scripts/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../stylesheets/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
