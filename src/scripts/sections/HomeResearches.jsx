1f1ee7e19d0bab2afde41825f0e69658d9107028
Matin_ <matinnikookar@gmail.com>
2020-10-21 18:31:28 +0800

    research part is almost done

--- src/scripts/sections/HomeResearches.jsx
+++ src/scripts/sections/HomeResearches.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   motion,
-  useSpring,
+  useCycle,
   useTransform,
   useViewportScroll,
 } from "framer-motion";
@@ -12,7 +12,27 @@ import Image from "../components/Image";
 import database from "../utils/database";
 import { ease, white, black } from "../utils/config";
 
+const rhombus = "polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)";
+const pentagon = "polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)";
+const hexagon = "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)";
+const heptagon =
+  "polygon(50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%)";
+const octagon =
+  "polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%)";
+
 const { researches } = database;
+const transition = { ease, delay: 0.2 };
+const imageVariants = {
+  initial: {
+    scaleY: 0,
+    opacity: 0,
+  },
+  animate: {
+    scaleY: 1,
+    opacity: [0, 1],
+    transition: transition,
+  },
+};
 
 function ListItem({ ...props }) {
   return (
@@ -24,6 +44,7 @@ function ListItem({ ...props }) {
       onHoverStart={props.onHoverStart}
       onHoverEnd={props.onHoverEnd}
       className="menu__list--item"
+      style={{ fontSize: "2.1rem" }}
     >
       <Link to={props.id} key={props.index}>
         {`0${props.index + 1}. ${props.title}`}
@@ -33,38 +54,68 @@ function ListItem({ ...props }) {
 }
 
 const HomeResearches = () => {
+  const { scrollYProgress } = useViewportScroll();
+  const { ref, inView } = useInView({ triggerOnce: true });
   const [currentImage, setCurrentImage] = useState([
     "capture.jpg",
     "capture.jpg",
   ]);
-  const { scrollYProgress } = useViewportScroll();
+  const [shape, cycleShape] = useCycle(
+    rhombus,
+    pentagon,
+    hexagon,
+    heptagon,
+    octagon
+  );
+  const [shapeRotate, cycleShapeAngle] = useCycle(0, 90, 180, 270, 360);
+  const [currentYear, setCurrentYear] = useState(2017);
   const rectPath = useTransform(scrollYProgress, [0.87, 1], [0.2, 1]);
-  const { ref: titleRef, inView: titleInView } = useInView({
-    triggerOnce: true,
-  });
+  const listPos = useTransform(scrollYProgress, [0.6, 1], [1000, -2000]);
+  const shapePos = useTransform(scrollYProgress, [0.6, 1], [100, 0]);
+  const imageScale = useTransform(scrollYProgress, [0.6, 1], [0.95, 1.1]);
+
   return (
-    <motion.section className="home__researches">
-      <h2 ref={titleRef} className="home__researches--title">
-        <AnimatedText isVisible={titleInView}>RESEARCHES</AnimatedText>
+    <motion.section
+      transition={transition}
+      animate="animate"
+      initial="intial"
+      className="home__researches"
+    >
+      <h2 ref={ref} className="home__researches--title">
+        <AnimatedText isVisible={inView}>RESEARCHES</AnimatedText>
       </h2>
       <div className="home__researches--container">
         <Image
-          containerClass="home__researches--image-container"
-          imageClass="home__researches--image"
+          containerStyle={{ scale: imageScale, y: listPos }}
+          imageVariants={imageVariants}
+          containerClass="home__researches__image-container"
+          imageClass="home__researches__image"
           main={currentImage[0]}
           compressed={currentImage[1]}
         />
-        <ul className="home__researches__list">
+
+        <motion.ul style={{ y: listPos }} className="home__researches__list">
           {researches.map((item, index) => (
             <ListItem
-              onHoverStart={() => setCurrentImage([item.image, item.image])}
-              // onHoverEnd={() => setCurrentImage("")}
+              onHoverStart={() => {
+                cycleShape();
+                cycleShapeAngle();
+                setCurrentYear(item.year);
+                setCurrentImage([item.image, item.image]);
+              }}
               title={item.title}
               id={item.id}
               index={index}
             />
           ))}
-        </ul>
+        </motion.ul>
+        <motion.div
+          transition={transition}
+          style={{ clipPath: shape, rotate: shapeRotate, x: shapePos }}
+          className="home__researches--shape"
+        >
+          {currentYear}
+        </motion.div>
         <svg className="home__researches--rect" width="600" height="600">
           <motion.path
             style={{ pathLength: rectPath }}
--- src/scripts/utils/database.js
+++ src/scripts/utils/database.js
@@ -1,16 +1,46 @@
 export default {
   researches: [
-    { id: 0, title: "Augmented Reality with Fologram", image: "./capture.jpg" },
-    { id: 1, title: "Terrace Apartment", image: "./main.jpg" },
-    { id: 2, title: "Woven Wall Panels", image: "./capture-2.jpg" },
-    { id: 3, title: "Al-Bahr Towers", image: "./main.jpg" },
-    { id: 4, title: "The Infinite Line Pavilion", image: "./capture.jpg" },
+    {
+      id: 0,
+      title: "Augmented Reality with Fologram",
+      image: "./capture.jpg",
+      year: "2018",
+    },
+    { id: 1, title: "Terrace Apartment", image: "./main.jpg", year: "2014" },
+    {
+      id: 2,
+      title: "Woven Wall Panels",
+      image: "./capture-2.jpg",
+      year: "2019",
+    },
+    { id: 3, title: "Al-Bahr Towers", image: "./main.jpg", year: "1953" },
+    {
+      id: 4,
+      title: "The Infinite Line Pavilion",
+      image: "./capture.jpg",
+      year: "2015",
+    },
   ],
   designs: [
-    { id: 0, title: "Augmented Reality with Fologram", image: "./capture.jpg" },
-    { id: 1, title: "Terrace Apartment", image: "./main.jpg" },
-    { id: 2, title: "Woven Wall Panels", image: "./capture-2.jpg" },
-    { id: 3, title: "Al-Bahr Towers", image: "./main.jpg" },
-    { id: 4, title: "The Infinite Line Pavilion", image: "./capture.jpg" },
+    {
+      id: 0,
+      title: "Augmented Reality with Fologram",
+      image: "./capture.jpg",
+      year: "2013",
+    },
+    { id: 1, title: "Terrace Apartment", image: "./main.jpg", year: "2021" },
+    {
+      id: 2,
+      title: "Woven Wall Panels",
+      image: "./capture-2.jpg",
+      year: "2020",
+    },
+    { id: 3, title: "Al-Bahr Towers", image: "./main.jpg", year: "2018" },
+    {
+      id: 4,
+      title: "The Infinite Line Pavilion",
+      image: "./capture.jpg",
+      year: "2019",
+    },
   ],
 };
