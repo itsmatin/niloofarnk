ed3b2021183b8a946ea5447ca407b511f12ba4fa
Matin Nikookar <matinnikookar@gmail.com>
2021-01-05 13:31:37 +0800

    database updated

--- src/scripts/sections/ProjectDetails.jsx
+++ src/scripts/sections/ProjectDetails.jsx
@@ -29,53 +29,70 @@ const ProjectDetails = ({ bgColor, project }) => {
   });
   return (
     <>
-      <div className="project__details">
-        <motion.div
-          transition={{ ease: ease }}
-          variants={shapeVariants}
-          style={{ y: triangleY }}
-          initial="grey"
-          animate={bgColor === "white" ? "black" : "grey"}
-          className="project__shape project__shape--triangle"
-        />
-        <motion.div
-          initial="white"
-          animate={bgColor === "white" ? "black" : "white"}
-          variants={textVariants}
-          className="project__details__text"
-        >
-          <h1 className="project__details--title">More details</h1>
-          <p>{project.detailsDescription1}</p>
-          <br />
-          <p>{project.detailsDescription2}</p>
-        </motion.div>
-        <Image
-          containerStyle={{ scale: scaleSpring }}
-          containerClass="project__details--image"
-          alt={`${project.title} - ${project.type} Niloofar Nikookar`}
-          src={project.images.details1}
-        />
-      </div>
-      <div className="project__details" style={{ alignItems: "center" }}>
-        <Image
-          containerStyle={{ scale: scaleSecondSpring }}
-          transition={{ delay: 2 }}
-          containerClass="project__details--image"
-          alt={`${project.title} - ${project.type} Niloofar Nikookar`}
-          src={project.images.details2}
-        />
-        <div className="project__shape project__shape--octagon" />
-        <motion.div
-          initial="white"
-          animate={bgColor === "white" ? "black" : "white"}
-          variants={textVariants}
-          className="project__details__text"
-        >
-          <p>{project.detailsDescription3}</p>
-          <br />
-          <p>{project.detailsDescription4}</p>
-        </motion.div>
-      </div>
+      {project.details?.map((item, index) => {
+        return index % 2 === 0 ? (
+          <div key={index} className="project__details">
+            <motion.div
+              transition={{ ease: ease }}
+              variants={shapeVariants}
+              style={{ y: triangleY }}
+              initial="grey"
+              animate={bgColor === "white" ? "black" : "grey"}
+              className="project__shape project__shape--triangle"
+            />
+            <motion.div
+              initial="white"
+              animate={bgColor === "white" ? "black" : "white"}
+              variants={textVariants}
+              className="project__details__text"
+            >
+              {index === 0 && item.text && (
+                <h1 className="project__details--title">More details</h1>
+              )}
+              {item.text?.map((text, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    <p>{text}</p>
+                    <br />
+                  </React.Fragment>
+                );
+              })}
+            </motion.div>
+            <Image
+              containerStyle={{ scale: scaleSpring }}
+              containerClass="project__details--image"
+              alt={`${project.title} - ${project.type} - Niloofar Nikookar`}
+              src={item.image}
+            />
+          </div>
+        ) : (
+          <div className="project__details" style={{ alignItems: "center" }}>
+            <Image
+              containerStyle={{ scale: scaleSecondSpring }}
+              transition={{ delay: 2 }}
+              containerClass="project__details--image"
+              alt={`${project.title} - ${project.type} Niloofar Nikookar`}
+              src={item.image}
+            />
+            <div className="project__shape project__shape--octagon" />
+            <motion.div
+              initial="white"
+              animate={bgColor === "white" ? "black" : "white"}
+              variants={textVariants}
+              className="project__details__text"
+            >
+              {item.text?.map((text, index) => {
+                return (
+                  <React.Fragment key={index}>
+                    <p>{text}</p>
+                    <br />
+                  </React.Fragment>
+                );
+              })}
+            </motion.div>
+          </div>
+        );
+      })}
     </>
   );
 };
