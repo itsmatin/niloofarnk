caa1d8e7b01b2724c7a9963df53c567c25bdc73f
Matin Nikookar <matinnikookar@gmail.com>
2021-01-08 17:03:36 +0800

    female heteropia text removed

--- src/scripts/utils/database.js
+++ src/scripts/utils/database.js
@@ -160,10 +160,10 @@ export default {
       time: "Spring",
       year: "2017",
 
-      headerDescription:
-        "An architectural subversion of the genderbased segregation of public space in Tehran",
-      infoDescription:
-        "This project addresses the experiences of women in public space in Tehran, Iran by analyzing the spatial power dynamics in society and critiquing them in a radical fashion. ",
+      headerDescription: "",
+      // "An architectural subversion of the genderbased segregation of public space in Tehran",
+      infoDescription: "",
+      // "This project addresses the experiences of women in public space in Tehran, Iran by analyzing the spatial power dynamics in society and critiquing them in a radical fashion. ",
 
       images: {
         header: "/capstone/heterotopia-header.jpg",
@@ -179,49 +179,49 @@ export default {
         {
           image: "/capstone/heterotopia-details1.jpg",
           text: [
-            `Along College Bridge to Valiasr Square there are several
-      universities and cultural centers. Enghelab street is the book store
-      district in the city. These locations attract a significant population
-      of students and academics. It is, therefore, the starting point for
-      many political and social protests. This attitude is reflected in
-      cafes and other gathering spaces in the area. These spaces play
-      the role of counter-spaces, and develop a culture of resistance
-      to the power. The site is an old theatre --Cinema Enghelab-- on
-      Enghelab street.`,
-            `The site is chosen because it holds a memory specific to the area
-      and is surrounded by spaces representing both sides of power:
-      where segregation is intensified and where counter-spaces are
-      developed.
-      This segregation-power matrix was developed to analyze and
-      identify methods and devices of spatial segregation through
-      architecture. It was used to find the weak zones where the
-      segregation line is not rigid. The matrix decodes power structures,
-      hierarchy within each space, and the experience of users within
-      them.`,
+            //       `Along College Bridge to Valiasr Square there are several
+            // universities and cultural centers. Enghelab street is the book store
+            // district in the city. These locations attract a significant population
+            // of students and academics. It is, therefore, the starting point for
+            // many political and social protests. This attitude is reflected in
+            // cafes and other gathering spaces in the area. These spaces play
+            // the role of counter-spaces, and develop a culture of resistance
+            // to the power. The site is an old theatre --Cinema Enghelab-- on
+            // Enghelab street.`,
+            //       `The site is chosen because it holds a memory specific to the area
+            // and is surrounded by spaces representing both sides of power:
+            // where segregation is intensified and where counter-spaces are
+            // developed.
+            // This segregation-power matrix was developed to analyze and
+            // identify methods and devices of spatial segregation through
+            // architecture. It was used to find the weak zones where the
+            // segregation line is not rigid. The matrix decodes power structures,
+            // hierarchy within each space, and the experience of users within
+            // them.`,
           ],
         },
         {
           image: "/capstone/heterotopia-details2.jpg",
           text: [
-            `The design emphasizes experiences that exist in social space
-            as a result of gendered power dynamics.
-            From the outside, the building is a simple box, not revealing the
-            interior scheme. This holds the memory of the theatre’s form,
-            and emphasizes the concept of a heterotopia by hiding in plain
-            sight.
-            There is a line running throughout the building, separating
-            spaces and emotions. It’s the extruded line of segregation
-            in Iran that does not always have physical form. The design
-            takes that line, twists it, breaks and manipulates it. The line is
-            produced in conjunction with the program of each space. `,
-            `Depending on whether the space is It takes the form of
-            furniture, openings, corridors shelves and etc in the building. It
-            does not touch the perimeter of the building. The space creates
-            the feeling of being excluded, watched, oppressed or limited.
-            Each space is tuned to demonstrate spatial power dynamics in
-            order to make them obvious and indefensible. 
-            Each program demonstrates segregation in a different way. The book store divides books by stereotypical gendered readership while breaking the lines between the shelves. Similarly, the gallery divides spaces into those dedicated to male and female artists. Cafes and game cafes unify or divide users, and other uses, like the socializing and lobby spaces, make use of sight and vantage to create power dynamics in the space. The space stretches out to the rooftop, where only pieces of the line remain.
-            `,
+            // `The design emphasizes experiences that exist in social space
+            // as a result of gendered power dynamics.
+            // From the outside, the building is a simple box, not revealing the
+            // interior scheme. This holds the memory of the theatre’s form,
+            // and emphasizes the concept of a heterotopia by hiding in plain
+            // sight.
+            // There is a line running throughout the building, separating
+            // spaces and emotions. It’s the extruded line of segregation
+            // in Iran that does not always have physical form. The design
+            // takes that line, twists it, breaks and manipulates it. The line is
+            // produced in conjunction with the program of each space. `,
+            // `Depending on whether the space is It takes the form of
+            // furniture, openings, corridors shelves and etc in the building. It
+            // does not touch the perimeter of the building. The space creates
+            // the feeling of being excluded, watched, oppressed or limited.
+            // Each space is tuned to demonstrate spatial power dynamics in
+            // order to make them obvious and indefensible.
+            // Each program demonstrates segregation in a different way. The book store divides books by stereotypical gendered readership while breaking the lines between the shelves. Similarly, the gallery divides spaces into those dedicated to male and female artists. Cafes and game cafes unify or divide users, and other uses, like the socializing and lobby spaces, make use of sight and vantage to create power dynamics in the space. The space stretches out to the rooftop, where only pieces of the line remain.
+            // `,
           ],
         },
       ],
